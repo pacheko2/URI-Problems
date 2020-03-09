24dc5528dfaacdd051e1a834e5e5c70bc8cecb72
pacheko2 <61974985+pacheko2@users.noreply.github.com>
2020-03-09 10:26:44 -0300

    Delete 1001.js

--- 1001.js
+++
@@ -1,8 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-var a = parseInt(lines.shift());
-var b = parseInt(lines.shift());
-
-var x = a+b
-console.log("X = " + x)
